2e776b37d3802fc5bf662b76785177d04e8ff178
w1r3d3 <42114063+w1r3d3@users.noreply.github.com>
2018-09-07 23:26:15 +0200

    Update bandcamp-dl.pl

--- bandcamp-dl.pl
+++ bandcamp-dl.pl
@@ -10,12 +10,21 @@
 # Bandcamp Downloader v1
 # USAGE: bandcamp_dl.pl ARG
 #
-# ARG can be an URL to a Bandcamp album, e.g. 'https://woodenshjips.bandcamp.com/album/v' or a file path to a local image of your Bandcamp fan page, e.g. 'fanpage.html'.
+# ARG can be an URL to a Bandcamp album, e.g. 'https://woodenshjips.bandcamp.com/album/v', or a file path to a local image of your Bandcamp fan page, e.g. 'fanpage.html'.
 # To generate such an image file, navigate Chrome to your Bandcamp fan page 'https://bandcamp.com/YOUR-FAN-ACCOUNT-NAME' and step through all lists until all items was
 # fetched/loaded. Then store the entire web page as a complete/full HTML file and use this file as the input argument to this tool.
 #
+use Encode;
 use JSON;
 use LWP::Simple;
+
+sub convert_filename($)
+{
+	($fname) = @_;
+	$fname =~ s/[\:\*\\\/\?\|<>"]/_/g;
+	$fname =~ s/^[\s\.]+|[\s\.]+$//g;
+	return encode('cp1252', $fname);
+}
 
 sub download_file($$)
 {
@@ -45,74 +54,78 @@ sub download_album($)
 {
 	($album_url) = @_;
 	
-	$input = get($album_url) or die "ERROR: Failed to fetch input data from '$album_url'";
-	$input =~ m/^\s+artist: \"(.+)\"/m;
-	$artist = $1;
-	$input =~ m/^\s+album_title: \"(.+)\"/m;
-	$title = $1;
-	$input =~ m/^\s+art_id: (\d+)/m;
-	$cover = $1;
-	$input =~ m/^\s+trackinfo\s?:(.+),$/m;
-	$trackinfo = from_json($1);
-	
-	$album = "$artist - $title";
-	$album =~ s/[^A-Za-z0-9\-\.\s']/_/g;
-	print "Download album '$album'...\n";
+	$input = get($album_url);
+	if ($input)
 	{
-		mkdir $album;
+		$input =~ m/^\s+artist:\s*\"(.+)\",/m;
+		$artist = $1;
+		$input =~ m/^\s+album_title:\s*\"(.+)\",/m;
+		$title = $1;
+		$input =~ m/^\s+art_id:\s*(\d+)\s*,/m;
+		$cover = $1;
+		$input =~ m/^\s+trackinfo\s?:(.+),$/m;
+		$trackinfo = from_json($1);
+		$folder = convert_filename($artist.' - '.$title);
+		
+		print "\tAlbum: '$artist' - '$title'\n";
+		
+		print "\tFolder: $folder\n";
+		mkdir $folder;
+		
+		download_file($folder.'/cover.jpg', 'https://f4.bcbits.com/img/a'.$cover.'_10.jpg');
+		
 		for (@$trackinfo)
 		{
 			$url = $_->{file}->{'mp3-128'};
-			$url =~ s\^//\http://\;
-			$title = "$_->{title}";
-			$title =~ s/[^A-Za-z0-9\-\.\s']/_/g;
-			$fname = sprintf("$album/%02d $title.mp3", $_->{track_num});
-			download_file($fname, $url);
+			$url =~ s\^//\https?://\;
+			$title = $_->{title};
+			$track = sprintf('%02d', $_->{track_num});
+			download_file($folder.'/'.convert_filename("$track - $title.mp3"), $url);
 		}
-		
-		$url = 'https://f4.bcbits.com/img/a'.$cover.'_10.jpg';
-		$fname = "$album/cover.jpg";
-		download_file($fname, $url);
+	}
+	else
+	{
+		print "ERROR: Failed to fetch data from '$album_url'\n";
 	}
 }
 
 sub main($)
 {
 	($arg) = @_;
-
-	die "ERROR: Invalid arguments!\n" if not $arg;
-
-	local $/ = undef;
-	open FILE, $arg;
-	if (defined FILE)
+	die "ERROR: No argument specified" if not $arg;
+	
+	binmode STDOUT, ":encoding(cp850)";
+	
+	if ($arg =~ /^https?:/)
 	{
-		binmode FILE;
-		$input = <FILE>;
-		close FILE;
+		print "Download album from '$arg'\n";
+		download_album($arg);
+	}
+	else
+	{
+		%albums = ();
 		
-		%albums=();
-		for ($input =~ m/^\s*<a target=\"_blank\" href=\"(.+)\" class=\"item-link\">/gm)
+		open($in, '<', $arg) or die "ERROR: Failed to open input file '$arg'";
+		while (<$in>)
 		{
-			$albums{$_}++ or $albums{$_}=1;
+			chomp;
+			$albums{$1}=1 if (m/^\s*<a target=\"_blank\" href=\"(.+)\" class=\"item-link\">/gm);
 		}
+		close($in);
 		
 		$albums_sz = scalar keys %albums;
 		print "Found $albums_sz albums on page '$arg'\n";
+		
 		if ($albums_sz > 0)
 		{
-			print "Start downloading now!\n";
 			$cnt = 0;
 			for (sort keys %albums)
 			{
-				print sprintf("[%.0f%%] Download album %d of %d", (100.0*$cnt)/$albums_sz, $cnt+1, $albums_sz)." from '$_'\n";
+				print sprintf("[%d%%] Download album %d of %d", (100*$cnt)/$albums_sz, $cnt+1, $albums_sz)." from '$_'\n";
 				download_album($_);
 				$cnt++;
 			}
 		}
-	}
-	else
-	{
-		download_album($arg);
 	}
 }
 
